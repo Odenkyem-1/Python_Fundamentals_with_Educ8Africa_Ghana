3e27e18b3e21e6b670f853fc157064b3d743ae62
Lloyd's Project <149918002+Odenkyem-1@users.noreply.github.com>
2025-11-16 20:08:04 +0000

    Add files via upload

---
+++ Name_Cases.py
@@ -0,0 +1,4 @@
+name = "Lloyd Kwadwo Agyapong"
+print(name.title())
+print(name.lower())
+print(name.upper())
---
+++ famous_quote_2.py
@@ -0,0 +1,4 @@
+famous_person = "J. Cole"
+message = "Love yours"
+
+print(f'{famous_person} once said, "{message}".')
