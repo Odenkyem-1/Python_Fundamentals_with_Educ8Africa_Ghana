813b65ef869c34e254e9f0c13de31d64e0072f55
Lloyd's Project <149918002+Odenkyem-1@users.noreply.github.com>
2025-12-06 14:58:23 +0000

    Add files via upload

---
+++ Eric Matthes 2nd Edition/Lists/greetings.py
@@ -0,0 +1,6 @@
+names = ["Mami", "Abena", "Akwasi", "Jedi", "Shulamite"]
+print(f'How are doing {names[0]}?')
+print(f'How are doing {names[1]}?')
+print(f'How are doing {names[2]}?')
+print(f'How are doing {names[3]}?')
+print(f'How are doing {names[4]}?')
