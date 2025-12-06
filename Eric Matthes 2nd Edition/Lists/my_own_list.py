a138411ad1a0564d4ffb49ffb4b2fb22c9960a2a
Lloyd's Project <149918002+Odenkyem-1@users.noreply.github.com>
2025-12-06 15:02:55 +0000

    Add files via upload

---
+++ Eric Matthes 2nd Edition/Lists/my_own_list.py
@@ -0,0 +1,2 @@
+my_cars = ["BMW M3", "BMW M5", "Toyota Supra", "Benz G-Wagon"]
+print(f'In the future, I would like to own a {my_cars[0]}, a {my_cars[1]}, a {my_cars[2]} and finally a {my_cars[3]}')
