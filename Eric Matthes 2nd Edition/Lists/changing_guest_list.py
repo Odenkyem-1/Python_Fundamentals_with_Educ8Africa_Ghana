ae718d445a78bf82039aed6b419344ce3c06581f
Lloyd's Project <149918002+Odenkyem-1@users.noreply.github.com>
2025-12-07 18:50:53 +0000

    Add files via upload

---
+++ Eric Matthes 2nd Edition/Lists/changing_guest_list.py
@@ -0,0 +1,7 @@
+guests = ['Odura', 'Nhyira', 'Clifford']
+print(f'Hello everyone, Mrs. {guests[0]} will not make it to dinner. Sorry for the inconvince')
+
+guests[0] = 'Mami'
+print(f'\nMr. {guests[2]}, you are invited to dinner at my house')
+print(f'Mrs. {guests[1]}, you are invited to dinner at my house')
+print(f'Mrs. {guests[0]}, you are invited to dinner at my house')
---
+++ Eric Matthes 2nd Edition/Lists/guest_list.py
@@ -0,0 +1,4 @@
+guests = ['Odura', 'Nhyira', 'Clifford']
+print(f'Mr. {guests[2]}, you are invited to dinner at my house')
+print(f'Mrs. {guests[1]}, you are invited to dinner at my house')
+print(f'Mrs. {guests[0]}, you are invited to dinner at my house')
