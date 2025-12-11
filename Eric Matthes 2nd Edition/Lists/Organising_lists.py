4f7b38ab596530c7b1c8149386f5d99375016e48
Lloyd's Project <149918002+Odenkyem-1@users.noreply.github.com>
2025-12-11 14:32:44 +0000

    Add files via upload

---
+++ Eric Matthes 2nd Edition/Lists/Organising_lists.py
@@ -0,0 +1,6 @@
+cars = ['toyota', 'subaru', 'lambogheni', 'audi', 'mazda']
+cars.sort(reverse=True)
+
+numbers=[1,2,3,6,5,7,4,9]
+numbers.reverse()
+print(numbers)
---
+++ Eric Matthes 2nd Edition/Lists/seeing_the_world.py
@@ -0,0 +1,32 @@
+locations = ['United Kingdom', 'United States of America', 'Ireland', 'China', 'Japan']
+
+print("Original list: ")
+print(locations)
+
+print("Sorted list: ")
+print(sorted(locations))
+
+print("Original list: ")
+print(locations)
+
+print("\nSorted list(reverse): ")
+print(sorted(locations, reverse=True))
+
+print("Original list: ")
+print(locations)
+
+print("\nSorted list(reverse): ")
+locations.reverse()
+print(locations)
+
+print("\nSorted list(reverse): ")
+locations.reverse()
+print(locations)
+
+print("\nSorted list(sort): ")
+locations.sort()
+print(locations)
+
+print("\nSorted list(sort): ")
+locations.sort(reverse=True)
+print(locations)
